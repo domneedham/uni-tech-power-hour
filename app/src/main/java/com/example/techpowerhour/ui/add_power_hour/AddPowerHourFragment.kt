39f8807518ef84f23c5a02ecaddb8863f162aece
domneedham <34138267+domneedham@users.noreply.github.com>
2021-03-14 10:43:35 +0000

    Added some basic form validation for creating a new pour hour

--- app/src/main/java/com/example/techpowerhour/ui/add_power_hour/AddPowerHourFragment.kt
+++ app/src/main/java/com/example/techpowerhour/ui/add_power_hour/AddPowerHourFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -16,6 +17,7 @@ import com.example.techpowerhour.data.model.enums.PowerHourType
 import com.example.techpowerhour.databinding.FragmentAddPowerHourBinding
 import com.example.techpowerhour.util.DatePickerHelper
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -71,22 +73,33 @@ class AddPowerHourFragment : Fragment() {
 
     private fun setupSaveButtonBinding() {
         binding.floatingActionButton.setOnClickListener {
-            val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-            val date = LocalDate.parse(binding.datePickerText.text.toString(), formatter)
+            val nameText = binding.workoutNameText.text.toString().trim()
+            val durationText = binding.durationText.text.toString().trim()
+            val typeText = binding.typeText.text.toString().trim()
+            val dateText = binding.datePickerText.text.toString().trim()
 
-            viewModel.createNewPourHour(
-                binding.workoutNameText.text.toString(),
-                binding.durationText.text.toString().toDouble(),
-                PowerHourType.valueOf(binding.typeText.text.toString()),
-                date
-            )
+            resetFormErrors()
 
-            resetForm()
-            Snackbar.make(requireContext(), requireView(), "Power hour created!", Snackbar.LENGTH_SHORT).show()
+            val errors = checkForFormErrors(nameText, durationText, typeText, dateText)
 
-            // go back to previous fragment
-            // can ignore if fails, leave user to navigate
-            findNavController().navigateUp()
+            if (!errors) {
+                val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+                val date = LocalDate.parse(dateText, formatter)
+
+                viewModel.createNewPourHour(
+                        nameText,
+                        durationText.toDouble(),
+                        PowerHourType.valueOf(typeText),
+                        date
+                )
+
+                resetForm()
+                Snackbar.make(requireContext(), requireView(), "Power hour created!", Snackbar.LENGTH_SHORT).show()
+
+                // go back to previous fragment
+                // can ignore if fails, leave user to navigate
+                findNavController().navigateUp()
+            }
         }
     }
 
@@ -108,6 +121,8 @@ class AddPowerHourFragment : Fragment() {
     }
 
     private fun resetForm() {
+        resetFormErrors()
+
         binding.workoutNameText.text?.clear()
         binding.workoutNameText.clearFocus()
         binding.durationText.text?.clear()
@@ -116,5 +131,60 @@ class AddPowerHourFragment : Fragment() {
         binding.typeText.clearFocus()
         binding.datePickerText.text?.clear()
         binding.datePickerText.clearFocus()
+    }
+
+    private fun resetFormErrors() {
+        binding.workoutNameLayout.error = null
+        binding.durationLayout.error = null
+        binding.typeLayout.error = null
+        binding.datePickerLayout.error = null
+    }
+
+    private fun checkForFormErrors(nameText: String, durationText: String, typeText: String, dateText: String): Boolean {
+        // check for issues in the form
+        var errors = false
+        if (checkForNameError(nameText)) {
+            binding.workoutNameLayout.error = "A name is missing"
+            errors = true
+        }
+
+        if (checkForDurationError(durationText)) {
+            binding.durationLayout.error = "The workout duration is missing"
+            errors = true
+        }
+
+        if (checkForTypeError(typeText)) {
+            binding.typeLayout.error = "The workout type is missing"
+            errors = true
+        }
+
+        if (checkForDateError(dateText)) {
+            binding.datePickerLayout.error = "The date of the workout is missing"
+            errors = true
+        }
+
+        return errors
+    }
+
+    private fun checkForNameError(text: String): Boolean {
+        if (text.isEmpty()) return true
+        return false
+    }
+
+    private fun checkForDurationError(text: String): Boolean {
+        if (text.isEmpty() || text.toDoubleOrNull() == null) {
+            return true
+        }
+        return false
+    }
+
+    private fun checkForTypeError(text: String): Boolean {
+        if (text.isEmpty()) return true
+        return false
+    }
+
+    private fun checkForDateError(text: String): Boolean {
+        if (text.isEmpty()) return true
+        return false
     }
 }

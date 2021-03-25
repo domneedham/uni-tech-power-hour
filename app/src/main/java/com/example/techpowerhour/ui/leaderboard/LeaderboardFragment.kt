0d30dc36507b3c6aaaecef5c371d56e934c25d9c
Dominic Needham <dominic.needham@bt.com>
2021-03-25 16:17:30 +0000

    initial filtering of dates based on date today and power hour listed date

--- app/src/main/java/com/example/techpowerhour/ui/leaderboard/LeaderboardFragment.kt
+++ app/src/main/java/com/example/techpowerhour/ui/leaderboard/LeaderboardFragment.kt
@@ -1,16 +1,15 @@
 package com.example.techpowerhour.ui.leaderboard
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.techpowerhour.R
 import com.example.techpowerhour.Repositories
+import com.example.techpowerhour.data.model.PowerHour
 import com.example.techpowerhour.databinding.FragmentLeaderboardBinding
-import com.example.techpowerhour.ui.user_power_hour_list.PowerHourRecyclerAdapter
 
 class LeaderboardFragment : Fragment() {
 
@@ -21,6 +20,37 @@ class LeaderboardFragment : Fragment() {
     private lateinit var viewModel: LeaderboardViewModel
 
     private lateinit var layoutManager: LinearLayoutManager
+
+    private var dateRange = DateRanges.TODAY
+
+    private lateinit var allPowerHours: List<PowerHour>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.leaderboard_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_date_range_today -> {
+                dateRange = DateRanges.TODAY
+            }
+            R.id.action_date_range_week -> {
+                dateRange = DateRanges.WEEK
+            }
+            R.id.action_date_range_month -> {
+                dateRange = DateRanges.MONTH
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+        displayFilteredPowerHours()
+        return true
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -44,12 +74,34 @@ class LeaderboardFragment : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory).get(LeaderboardViewModel::class.java)
     }
 
+    private fun displayFilteredPowerHours() {
+        val filteredPowerHours: List<PowerHour> = when (dateRange) {
+            DateRanges.TODAY -> {
+                allPowerHours.filter { it.epochDate!!.toInt() == 18710}
+            }
+            DateRanges.WEEK -> {
+                allPowerHours.filter { it.epochDate!!.toInt() in 18707..18710 }
+            }
+            DateRanges.MONTH -> {
+                allPowerHours.filter { it.epochDate!!.toInt() in 18698..18710}
+            }
+        }
+        val adapter = LeaderboardUserRecyclerAdapter(
+                filteredPowerHours,
+        )
+        binding.powerHourList.adapter = adapter
+    }
+
     private fun observePowerHourTable() {
         viewModel.getAllPowerHours().observe(viewLifecycleOwner, { powerHours ->
-            val adapter = LeaderboardUserRecyclerAdapter(
-                    powerHours,
-            )
-            binding.powerHourList.adapter = adapter
+            allPowerHours = powerHours
+            displayFilteredPowerHours()
         })
+    }
+
+    enum class DateRanges {
+        TODAY,
+        WEEK,
+        MONTH
     }
 }

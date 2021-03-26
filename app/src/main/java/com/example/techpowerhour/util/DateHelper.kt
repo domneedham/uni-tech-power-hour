eb697b65bde55c971e71693b051362376b8f4429
Dominic Needham <dominic.needham@bt.com>
2021-03-26 15:50:14 +0000

    Changed way errors are displayed on the form and now prevent a user from adding a date in the future

--- app/src/main/java/com/example/techpowerhour/util/DateHelper.kt
+++ app/src/main/java/com/example/techpowerhour/util/DateHelper.kt
@@ -2,9 +2,13 @@ package com.example.techpowerhour.util
 
 import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 object DateHelper {
+    val todayInMs = Calendar.getInstance().timeInMillis
     val todayEpoch = LocalDate.now().toEpochDay()
 
     private val differenceInDaysWeek = LocalDate.now().dayOfWeek.compareTo(DayOfWeek.MONDAY).toLong()
@@ -24,6 +28,34 @@ object DateHelper {
     }
 
     /**
+     * Parse an LocalDate into a UI friendly date
+     * @param localDate The date object to parse
+     */
+    fun displayDate(localDate: LocalDate): String? {
+        return displayDate(localDate.toEpochDay())
+    }
+
+    /**
+     * Parse day, month, year into a LocalDate
+     * @param day The day of the week
+     * @param month The month of the year
+     * @param year The year
+     */
+    fun parseDateToLocalDate(day: Int, month: Int, year: Int): LocalDate? {
+        return LocalDate.parse("$day-$month-$year", formatter)
+    }
+
+    /**
+     * Parse day, month, year into a LocalDate
+     * @param day The day of the week
+     * @param month The month of the year
+     * @param year The year
+     */
+    fun parseDateToLocalDate(day: String, month: String, year: String): LocalDate? {
+        return LocalDate.parse("$day-$month-$year", formatter)
+    }
+
+    /**
      * Parse a string in format "dd-MM-yyyy" to a LocalDate object
      * @param date The date text
      */
@@ -38,5 +70,4 @@ object DateHelper {
     fun parseDateToEpoch(date: String) : Long? {
         return parseDateToLocalDate(date)?.toEpochDay()
     }
-
 }

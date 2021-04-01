6cfacb4e3bf7170472d57d74c106734e4c8cda48
Dominic Needham <dominic.needham@bt.com>
2021-04-01 10:52:17 +0100

    Add user information to power hours

--- app/src/main/java/com/example/techpowerhour/ui/add_power_hour/AddPowerHourViewModel.kt
+++ app/src/main/java/com/example/techpowerhour/ui/add_power_hour/AddPowerHourViewModel.kt
@@ -6,18 +6,22 @@ import com.example.techpowerhour.data.model.PowerHour
 import com.example.techpowerhour.data.model.enums.PowerHourType
 import com.example.techpowerhour.data.repository.PowerHourRepository
 import com.example.techpowerhour.util.DateHelper
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class AddPowerHourViewModel(private val repository: PowerHourRepository) : ViewModel() {
+    private val auth = FirebaseAuth.getInstance()
+
     fun createNewPourHour(name: String, duration: String, type: String, date: String): PowerHour {
         val powerHour = PowerHour(
                 name,
                 duration.toDouble(),
                 PowerHourType.valueOf(type),
-                DateHelper.parseDateToEpoch(date)
+                DateHelper.parseDateToEpoch(date),
+                auth.uid
         )
         repository.insert(powerHour)
         return powerHour

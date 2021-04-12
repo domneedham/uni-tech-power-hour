08f543831931633495c7df14459233c7553a3aaa
Dominic Needham <dominic.needham@bt.com>
2021-04-12 10:16:10 +0100

    Improvement of services and repositories documentation

--- app/src/main/java/com/example/techpowerhour/Repositories.kt
+++ app/src/main/java/com/example/techpowerhour/Repositories.kt
@@ -5,37 +5,44 @@ import com.example.techpowerhour.data.repository.PowerHourRepository
 import com.example.techpowerhour.data.repository.StatisticsRepository
 import com.example.techpowerhour.data.repository.UserRepository
 
+/**
+ * Singleton for holding a reference to all of the repositories for the application.
+ * Each repository is lazy loaded.
+ */
 object Repositories {
     /**
-     * Holds the instance of the UserRepository class.
-     * The value is lazy loaded and will call the onInit method on first request.
+     * Holds the instance of the [UserRepository] class.
+     * The value is lazy loaded and will call the [UserRepository.onInit] method on first request.
      */
     val user = lazy {
         val ur = UserRepository(Services.user)
         ur.onInit()
         ur
     }
+
     /**
-     * Holds the instance of the PowerHourRepository class.
-     * The value is lazy loaded and will call the onInit method on first request.
+     * Holds the instance of the [PowerHourRepository] class.
+     * The value is lazy loaded and will call the [PowerHourRepository.onInit] method on first request.
      */
     val powerHour = lazy {
         val phr = PowerHourRepository(Services.powerHour)
         phr.onInit()
         phr
     }
+
     /**
-     * Holds the instance of the LeaderboardRepository class.
-     * The value is lazy loaded and will call the onInit method on first request.
+     * Holds the instance of the [LeaderboardRepository] class.
+     * The value is lazy loaded and will call the [LeaderboardRepository.onInit] method on first request.
      */
     val leaderboard = lazy {
         val lr = LeaderboardRepository(Services.leaderboard)
         lr.onInit()
         lr
     }
+
     /**
-     * Holds the instance of the StatisticsRepository class.
-     * The value is lazy loaded and will call the onInit method on first request.
+     * Holds the instance of the [StatisticsRepository] class.
+     * The value is lazy loaded and will call the [StatisticsRepository.onInit] method on first request.
      */
     val statistics = lazy {
         val sr = StatisticsRepository(Services.statistics)
@@ -44,7 +51,7 @@ object Repositories {
     }
 
     /**
-     * To be called when the MainActivity is loaded.
+     * To be called when the [MainActivity] is loaded.
      * On first load of the Activity, this method will do nothing as none of the repositories are initialised.
      * On subsequent loads (e.g. if a user does a logout and login), it will recall the onInit methods of the repositories.
      * This ensures cache values and variables are appropriately set for the user
@@ -57,7 +64,7 @@ object Repositories {
     }
 
     /**
-     * To be called when the MainActivity is destroyed.
+     * To be called when the [MainActivity] is destroyed.
      * This will call the onDestroy method of each repository.
      * The Activity is currently only destroyed if a user does a logout.
      * This method will do nothing to the repository if it has never been initialised thanks to lazy loading.
